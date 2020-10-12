540cd3dd163a8b21af41e112ccc27b602648f48a
Luis Jair Alvarado Tobias <luisjair1394@gmail.com>
2020-10-11 22:08:18 -0500

    Agregado de codigo

--- ptrct.py
+++ ptrct.py
@@ -1,1 +1,8 @@
-print("Hola mundo")
+lista=[]
+while True:
+    menu=int(input("Deseas continuar: "))
+    if menu ==1:
+        nombre=input("Dime un nombre: ")
+        lista.append(nombre)
+    else:
+        break
